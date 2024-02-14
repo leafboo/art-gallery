9137bdd772005cf75904fae22b65a2758494e6d1
Jan Romel <janjancaranguian@gmail.com>
2024-02-14 18:53:03 +0800

    Do profile picture module

--- src/pages/Artist_Profile/Details/Details.jsx
+++ src/pages/Artist_Profile/Details/Details.jsx
@@ -6,7 +6,7 @@ import Socials from "./Socials.jsx"
 export default function Details() {
   return (
     <div className={DetailsCSS['details-container']}>
-      <ProfilePicture />
+      <ProfilePicture profile="https://pbs.twimg.com/profile_images/1564831789625257984/W4F7yvkP_400x400.jpg"/>
       <Socials />
     </div>
     
--- src/pages/Artist_Profile/Details/ProfilePicture.jsx
+++ src/pages/Artist_Profile/Details/ProfilePicture.jsx
@@ -1,5 +1,11 @@
 import React from "react"
+import ProfilePictureCSS from "./styles/ProfilePicture.module.css"
 
-export default function ProfilePicture() {
-  return
+export default function ProfilePicture(props) {
+  return (
+    <div className={ProfilePictureCSS['profile-picture-container']}>
+      <img className={ProfilePictureCSS['profile-picture']} src={props.profile} alt="Profile Picture." />
+    </div>
+    
+  )
 }
