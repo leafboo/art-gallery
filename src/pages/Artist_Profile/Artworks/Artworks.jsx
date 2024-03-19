5fc1d1c99eb6fa5b2992ea65aba8d85953d27ffa
Jan romel <janjancaranguian@gmail.com>
2024-03-19 20:48:12 +0800

    Display the artworks of the artist on artist's profile page with the use of danbooru api

--- src/pages/Artist_Profile/Artworks/Artworks.jsx
+++ src/pages/Artist_Profile/Artworks/Artworks.jsx
@@ -4,8 +4,6 @@ import ArtworksCSS from "./styles/Artworks.module.css"
 
 export default function Artworks() {
   const [artistsData, setArtistsData] = React.useState([])
-
-  console.log('component rendered')
   React.useEffect(() => {
     async function fetchData() {
         const response = await fetch("https://danbooru.donmai.us/posts.json?tags=ciloranko")
@@ -15,21 +13,15 @@ export default function Artworks() {
     fetchData()
   }, [])
 
-  console.log(artistsData)
-
-  //const ArtistsElement = artistsData.map(artist => 
-  //  <Artwork image={artist}/>
-
-  //)
+  
+  const artworks = artistsData.map(data => data.media_asset.variants[4].url)
+  console.log(artworks)
+  const ArtistsElement = artworks.map( artwork => <Artwork image={artwork}/> )
 
   
   return (
     <div className={ArtworksCSS['artworks-container']}>
-      <Artwork image="https://pbs.twimg.com/media/E_rpq2gUUAMrYLj?format=jpg&name=large" />
-      <Artwork image="https://pbs.twimg.com/media/E9wtmhXVUAA9P7Z?format=jpg&name=4096x4096" />
-      <Artwork image="https://pbs.twimg.com/media/FO6O3j8acAALooF?format=jpg&name=4096x4096" />
-      <Artwork image="https://pbs.twimg.com/media/EsXJc23U0AE1dAh?format=jpg&name=large" />
-      <Artwork image="https://pbs.twimg.com/media/FOeloIJakAAmfLe?format=jpg&name=large" />
+      {ArtistsElement}
     </div>
   )
 }
