d8845911bab9ed55e0cfecf60654a57388ff04c7
Jan Romel <janjancaranguian@gmail.com>
2024-03-19 14:21:20 +0800

    Use danbooru api in artworks component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <div>
       <ArtistProfile />
+      {/*<FindArtist/ >*/}
       <DisplayMode />
     </div>
   )
--- src/pages/Artist_Profile/Artworks/Artworks.jsx
+++ src/pages/Artist_Profile/Artworks/Artworks.jsx
@@ -1,8 +1,26 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Artwork from "./Artwork.jsx"
 import ArtworksCSS from "./styles/Artworks.module.css"
 
 export default function Artworks() {
+  const [artistsData, setArtistsData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await fetch("https://danbooru.donmai.us/posts.json?tags=ciloranko")
+        const data = await response.json()
+        setArtistsData(data)
+      } catch(err) {
+        console.error(err)
+      }
+    }
+    fetchData()
+  }, [])
+
+  console.log(artistsData)
+
+  
   return (
     <div className={ArtworksCSS['artworks-container']}>
       <Artwork image="https://pbs.twimg.com/media/E_rpq2gUUAMrYLj?format=jpg&name=large" />
