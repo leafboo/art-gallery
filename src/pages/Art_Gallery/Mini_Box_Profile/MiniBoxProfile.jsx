cbfb9c90279a72bd31025ad5cc76f51f43d33b6f
Jan romel <janjancaranguian@gmail.com>
2024-03-22 02:58:02 +0800

    Display the artist's name in MiniBoxProfile

--- src/pages/Art_Gallery/Mini_Box_Profile/MiniBoxProfile.jsx
+++ src/pages/Art_Gallery/Mini_Box_Profile/MiniBoxProfile.jsx
@@ -5,6 +5,7 @@ export default function MiniBoxProfile() {
   return (
     <div className={MiniBoxProfileCSS['mini-box-profile-container']}>
       <img src="https://pbs.twimg.com/profile_images/1564831789625257984/W4F7yvkP_400x400.jpg" alt="Profile picture." className={MiniBoxProfileCSS['profile-picture']} />
+      <span className={MiniBoxProfileCSS['name']}>TrNyteal</span>
     </div>
   )
 }
