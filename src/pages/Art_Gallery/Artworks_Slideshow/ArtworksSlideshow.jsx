4f4a0716687b338cc19a69a2f196b49de2550a67
Jan Romel <janjancaranguian@gmail.com>
2024-03-21 10:17:20 +0800

    Center the artwork

--- src/pages/Art_Gallery/Artworks_Slideshow/ArtworksSlideshow.jsx
+++ src/pages/Art_Gallery/Artworks_Slideshow/ArtworksSlideshow.jsx
@@ -3,7 +3,7 @@ import ArtworksSlideshowCSS from "./ArtworksSlideshow.module.css"
 
 export default function ArtworksSlideshow() {
   return (
-    <div>
+    <div className={ArtworksSlideshowCSS['artworks-slideshow-container']}>
       <img src="https://pbs.twimg.com/media/E2xK73tVgAktY1q?format=jpg&name=4096x4096" alt="Artwork." className={ArtworksSlideshowCSS['artwork']} />
     </div>
   )
