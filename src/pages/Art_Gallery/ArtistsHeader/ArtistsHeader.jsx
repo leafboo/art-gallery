a4e09f7a0b3b4b79079e5d7a8bf954c0621340f4
Jan romel <janjancaranguian@gmail.com>
2024-03-22 03:20:40 +0800

    Start ArtistsHeader and make ArtistBlob

---
+++ src/pages/Art_Gallery/ArtistsHeader/ArtistsHeader.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ArtistsHeaderCSS from "./ArtistsHeader.module.css"
+
+export default function ArtistsHeader() {
+  return (
+    <div className={ArtistsHeaderCSS['artists-header-container']}>
+      
+    </div>
+  )
+}
