028c8af82796baf7276f747e73198f82d6166c15
Jan romel <janjancaranguian@gmail.com>
2024-03-22 08:23:20 +0800

    Style ArtistsHeader and ArtistBlob

--- src/pages/Art_Gallery/ArtistsHeader/Artist_Blob/ArtistBlob.jsx
+++ src/pages/Art_Gallery/ArtistsHeader/Artist_Blob/ArtistBlob.jsx
@@ -1,5 +1,10 @@
 import React from "react"
+import ArtistBlobCSS from "./ArtistBlob.module.css"
 
-export default function ArtistBlob() {
-  
+export default function ArtistBlob(props) {
+  return (
+    <div className={'artist-blob-container'}>
+      <img src={props.picture} alt="Profile picture." className={ArtistBlobCSS['profile-picture']} />
+    </div>
+  )
 } 
--- src/pages/Art_Gallery/ArtistsHeader/ArtistsHeader.jsx
+++ src/pages/Art_Gallery/ArtistsHeader/ArtistsHeader.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import ArtistsHeaderCSS from "./ArtistsHeader.module.css"
+import ArtistBlob from "./Artist_Blob/ArtistBlob.jsx"
 
 export default function ArtistsHeader() {
   return (
     <div className={ArtistsHeaderCSS['artists-header-container']}>
-      
+      <ArtistBlob picture="https://pbs.twimg.com/profile_images/1155680462464942081/JBCvjutU_400x400.jpg" />
+      <ArtistBlob picture="https://pbs.twimg.com/profile_images/1564831789625257984/W4F7yvkP_400x400.jpg" />
+      <ArtistBlob picture="https://pbs.twimg.com/profile_images/1466177220905295878/iwAMQRAw_400x400.jpg" />
+      <ArtistBlob picture="https://pbs.twimg.com/profile_images/1749147693732315136/-j1GU7R8_400x400.jpg" />
+      <ArtistBlob picture="https://pbs.twimg.com/profile_images/1360625679184797699/vS5rWDjv_400x400.jpg" />
     </div>
   )
 }
