31c8a2f6eb2fe521483330f41fa013fbf95a4b06
Jan romel <janjancaranguian@gmail.com>
2024-03-25 15:42:08 +0800

    Clean up Artworks component

--- src/pages/Artist_Profile/Artworks/Artworks.jsx
+++ src/pages/Artist_Profile/Artworks/Artworks.jsx
@@ -4,11 +4,13 @@ import ArtworksCSS from "./styles/Artworks.module.css"
 
 export default function Artworks(props) {
   const artworks = props.artistsData.map(data => data.media_asset.variants[4].url)
-  const ArtworksElement = artworks.map( (artwork, index) => <Artwork image={artwork} onImageClick={props.onImageClick} getImageIndex={props.getImageIndex} index={index} /> )
+  const ArtworksElement = artworks.map( (artwork, index) => 
+    <Artwork image={artwork} 
+             onImageClick={props.onImageClick} 
+             getImageIndex={props.getImageIndex} 
+             index={index} /> 
+  )
 
-
-
-  
   return (
     <div className={ArtworksCSS['artworks-container']}>
       {ArtworksElement}
