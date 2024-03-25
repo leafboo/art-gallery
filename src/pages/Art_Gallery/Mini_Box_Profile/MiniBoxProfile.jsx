662ad8f0b0d5ef99aeec945dbfc7946e35d42edf
Jan romel <janjancaranguian@gmail.com>
2024-03-25 16:42:33 +0800

    Clean up MiniBoxProfile component

--- src/pages/Art_Gallery/Mini_Box_Profile/MiniBoxProfile.jsx
+++ src/pages/Art_Gallery/Mini_Box_Profile/MiniBoxProfile.jsx
@@ -4,7 +4,11 @@ import MiniBoxProfileCSS from "./MiniBoxProfile.module.css"
 export default function MiniBoxProfile(props) {
   return (
     <div className={MiniBoxProfileCSS['mini-box-profile-container']}>
-      <img src="https://pbs.twimg.com/profile_images/1564831789625257984/W4F7yvkP_400x400.jpg" alt="Profile picture." className={MiniBoxProfileCSS['profile-picture']} onClick={props.onImageClick} />
+      <img src="https://pbs.twimg.com/profile_images/1564831789625257984/W4F7yvkP_400x400.jpg" 
+           alt="Profile picture." 
+           className={MiniBoxProfileCSS['profile-picture']} 
+           onClick={props.onImageClick} />
+           
       <span className={MiniBoxProfileCSS['name']}>TrNyteal</span>
     </div>
   )
