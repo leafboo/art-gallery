6b7ccbc2eacfe99287c612bf6eda9a2fafef2be7
Jan romel <janjancaranguian@gmail.com>
2024-03-25 11:25:44 +0800

    Add dark mode

--- src/pages/DisplayMode.jsx
+++ src/pages/DisplayMode.jsx
@@ -1,9 +1,25 @@
 import React from "react"
-import darkMode from "../assets/darkModeLogo.png"
+import darkModeLogo from "../assets/darkModeLogo.png"
+import lightModeLogo from "../assets/lightModeLogo.png"
 import DisplayModeCSS from "./DisplayMode.module.css"
 
+
 export default function DisplayMode() {
+  const [mode, setMode] = React.useState(true)
+
+  function changeMode() {
+    setMode(prevMode => !prevMode)
+  }
+  
+  React.useLayoutEffect(() => {
+    document.body.style.backgroundColor = mode ? 'white' : 'black'
+    document.body.style.color = mode ? 'black' : 'rgb(222, 222, 222)'
+  });
+
   return (
-    <img className={DisplayModeCSS['dark-mode-logo']} src={darkMode} alt="" />
+    <img  className={mode ? DisplayModeCSS['dark-mode-logo'] : DisplayModeCSS['light-mode-logo']} 
+          onClick={changeMode} 
+          src={mode ? darkModeLogo : lightModeLogo} 
+          alt="Display mode logo." />
   )
 }
