4f9b709aece2bbff659a30e9b6d616b6970403ff
Jan romel <janjancaranguian@gmail.com>
2024-03-25 14:58:12 +0800

    Clean up App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,12 @@ import ArtGallery from "./pages/Art_Gallery/ArtGallery.jsx"
 export default function App() {
   // switch between ArtGallery and ArtistProfile
   const [toggleComponent, setToggleComponent] = React.useState(true)
-
-  // save image index of clicked image in artists profile to display the image in art gallery
+  // save image index of clicked image to display in art gallery
   const [currentImageIndex, setCurrentImageIndex] = React.useState(parseInt(localStorage.getItem('currentImageIndex')) ?? 3)
+  // DATA FOR ARTISTS PROFILE
+  const [artistsData, setArtistsData] = React.useState([])
 
-  function getImageIndex(index) {
-    setCurrentImageIndex(index)
-    localStorage.setItem('currentImageIndex', index)
-  }
-
-
+  // retain page when refreshing
   React.useEffect(() => {
     const storedToggleComponent = localStorage.getItem('toggleComponent')
     if (storedToggleComponent !== null) {
@@ -26,9 +22,11 @@ export default function App() {
   }, [])
 
 
-  // DATA FOR ARTISTS PROFILE
-  const [artistsData, setArtistsData] = React.useState([])
-  
+  function getImageIndex(index) {
+    setCurrentImageIndex(index)
+    localStorage.setItem('currentImageIndex', index)
+  }
+
   function switchPages() {
     setToggleComponent(prevToggleComponent => !prevToggleComponent)
     // Store the updated state in localStorage
@@ -37,8 +35,17 @@ export default function App() {
   
   return (
     <div>
-      {toggleComponent ? (<ArtGallery onImageClick={switchPages} artistsData={artistsData} setArtistsData={setArtistsData} imageIndex={currentImageIndex} />) : (<ArtistProfile artistsData={artistsData} setArtistsData={setArtistsData} onImageClick={switchPages} getImageIndex={getImageIndex} />)}
-      
+      {toggleComponent ? (
+      <ArtGallery onImageClick={switchPages} 
+                  artistsData={artistsData} 
+                  setArtistsData={setArtistsData} 
+                  imageIndex={currentImageIndex} />
+      ) : (
+      <ArtistProfile artistsData={artistsData} 
+                     setArtistsData={setArtistsData} 
+                     onImageClick={switchPages} 
+                     getImageIndex={getImageIndex} />
+      )}
       {/*<ArtistProfile />*/}
       {/*<FindArtist/ >*/}
       <DisplayMode />
