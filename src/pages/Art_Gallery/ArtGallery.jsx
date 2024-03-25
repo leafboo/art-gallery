60867d319312bb2f8ca27eddc4484897280d89e7
Jan romel <janjancaranguian@gmail.com>
2024-03-25 15:57:12 +0800

    Clean up ArtGallery component

--- src/pages/Art_Gallery/ArtGallery.jsx
+++ src/pages/Art_Gallery/ArtGallery.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import ArtworksSlideshow from "./Artworks_Slideshow/ArtworksSlideshow.jsx"
-import MiniBoxProfile from "./Mini_Box_Profile/MiniBoxProfile.jsx"
 import ArtGalleryCSS from "./ArtGallery.module.css"
 
 export default function ArtGallery(props) {
@@ -15,7 +14,9 @@ export default function ArtGallery(props) {
   
   return (
     <div className={ArtGalleryCSS['art-gallery-container']}>
-      <ArtworksSlideshow onImageClick={props.onImageClick} artistsData={props.artistsData} imageIndex={props.imageIndex} />
+      <ArtworksSlideshow onImageClick={props.onImageClick} 
+                         artistsData={props.artistsData} 
+                         imageIndex={props.imageIndex} />
     </div>
   )
 }
