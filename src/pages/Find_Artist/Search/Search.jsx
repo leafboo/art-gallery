0a283000a23cfe4d2533fc127d7d38166bb3ffb3
Jan Romel <janjancaranguian@gmail.com>
2024-03-29 23:24:00 +0800

    Add debounce to search component

--- src/pages/Find_Artist/Search/Search.jsx
+++ src/pages/Find_Artist/Search/Search.jsx
@@ -4,29 +4,22 @@ import SearchCSS from "./Search.module.css"
 export default function Search(props) {
   const [name, setName] = React.useState('')
 
-  async function findArtist(name) {
-    let data = []
-    if (name) {
-      const response = await fetch(`https://danbooru.donmai.us/artists.json?search[any_other_name_like]=*${name}*`)
-      data = await response.json()
-    }
-    props.setFoundArtist(data)
-    
-  }
+  React.useEffect(() => {
+    const getArtist = setTimeout(async () => {
+      let data = []
+      if (name) {
+        const response = await fetch(`https://danbooru.donmai.us/artists.json?search[any_other_name_like]=*${name}*`)
+        data = await response.json()  
+      }
+      props.setFoundArtist(data)
+    }, 250)
 
-  function updateNameRealtime(event) {
-    const latestValue = event.target.value
-    setName(latestValue)
-    findArtist(latestValue)
-  }
-
-
-
-  console.log(name) // TEMPORARY
+    return () => clearTimeout(getArtist) 
+  }, [name])
 
   return (
     <div className={SearchCSS['search-container']}>
-      <input className={SearchCSS['search-bar']} type="text" value={name} onChange={updateNameRealtime} />
+      <input className={SearchCSS['search-bar']} type="text" value={name} onChange={(event) => { setName(event.target.value) }} />
       <img className={SearchCSS['magnifying-glass']} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/1200px-Magnifying_glass_icon.svg.png" alt="Magnifying glass." />
     </div>
   )
