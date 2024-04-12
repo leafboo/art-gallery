b7baac03c3bf5f3e0072b65c48932c4f6a5e9439
Jan romel <janjancaranguian@gmail.com>
2024-04-12 15:44:52 +0800

    Style the ImageNavigator arrow

--- src/pages/Art_Gallery/Artworks_Slideshow/Image_Navigator/ImageNavigator.jsx
+++ src/pages/Art_Gallery/Artworks_Slideshow/Image_Navigator/ImageNavigator.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ImageNavigatorCSS from "./ImageNavigator.module.css"
+import leftArrowBlack from "../../../../assets/leftArrowBlack.png"
+import rightArrowBlack from "../../../../assets/rightArrowBlack.png"
+import leftArrowWhite from "../../../../assets/leftArrowWhite.png"
+import rightArrowWhite from "../../../../assets/rightArrowWhite.png"
 
 export default function ImageNavigator(props) {
   return (
     <div className={ImageNavigatorCSS['image-navigator-container']}>
-      <div className={ImageNavigatorCSS['arrow-button']} onClick={() => {props.changeImageIndex(2)}} >&lt;</div>
-      <div className={ImageNavigatorCSS['arrow-button']} onClick={() => {props.changeImageIndex(1)}} >&gt;</div>
+      <div className={ImageNavigatorCSS['arrow-button']} onClick={() => {props.changeImageIndex(2)}} >
+        <img src={leftArrowBlack} alt="Left Arrow." className={ImageNavigatorCSS['arrow']} />
+      </div>
+      <div className={ImageNavigatorCSS['arrow-button']} onClick={() => {props.changeImageIndex(1)}} >
+        <img src={rightArrowBlack} alt="Right Arrow." className={ImageNavigatorCSS['arrow']} />
+      </div>
     </div>
   )
 }
